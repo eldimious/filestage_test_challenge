6f74eb40d8f6b59a6581ada08791511a2b661782
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-04-12 19:29:24 +0300

    Add forgotten regex validation

--- src/services/validationService.js
+++ src/services/validationService.js
@@ -1,11 +1,23 @@
 const {
-	startsWithAny,
-	SUPPORTED_SPORTS,
-} = require('../common/utils')
+  startsWithAny,
+  SUPPORTED_SPORTS,
+  BASKETBALL,
+  HANDBALL,
+} = require('../common/utils');
 
-function validateFile(data) {
+const playerNameFormat = 'player [0-9]+';
+const numeric = '[0-9]+';
+const space = ' +';
+const alphaNumeric = '[a-zA-Z0-9]*';
+const teamsFormat = '[AB]';
+const positionFormat = '[CGF]';
+const sportTypeRegex = new RegExp(`^${SUPPORTED_SPORTS.join('|')}\n`, 'i');
+const handballLineRegex = new RegExp(`^${playerNameFormat};${alphaNumeric};${numeric};Team${space}${teamsFormat};${positionFormat};${numeric};${numeric}$`, 'i');
+const basketballLineRegex = new RegExp(`^${playerNameFormat};${alphaNumeric};${numeric};Team${space}${teamsFormat};${positionFormat};${numeric};${numeric};${numeric}$`, 'i');
+
+function validateSportLineFormat(data) {
   try {
-    if (!startsWithAny(SUPPORTED_SPORTS, data)) {
+    if (!startsWithAny(SUPPORTED_SPORTS, data) || !sportTypeRegex.test(data)) {
       throw new Error('Add file with correct format.');
     }
     return undefined;
@@ -14,4 +26,21 @@ function validateFile(data) {
   }
 }
 
-module.exports.validateFile = validateFile;
+function validatePlayerLineFormat(sport, line) {
+  try {
+    if (sport === BASKETBALL && !basketballLineRegex.test(line)) {
+      throw new Error('Add file with correct format for players.');
+    } else if (sport === HANDBALL && !handballLineRegex.test(line)) {
+      throw new Error('Add file with correct format for players.');
+    }
+    return undefined;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = {
+  validateSportLineFormat,
+  validatePlayerLineFormat,
+};
